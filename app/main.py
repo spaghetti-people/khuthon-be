cf9f5a4b6a43d2afa6c88e2b68aafefc57cf6918
heung115 <heung115@naver.com>
2025-05-10 04:02:28 +0900

    feat: update models

--- app/main.py
+++ app/main.py
@@ -9,7 +9,7 @@ from starlette.middleware.sessions import SessionMiddleware
 from router.auth import router as auth_router
 from router.crop import router as crop_router
 from router.chat import router as chat_router
-
+from router.predict import router as predict_router
 
 from utils.db_driver import _init_db
 
@@ -20,16 +20,12 @@ load_dotenv()
 
 app = FastAPI()
 
-origins = [
-    'http://localhost:3000',
-    'http://localhost:8000',
-    'http://localhost:8080'
-]
+origins = ["http://localhost:3000", "http://localhost:8000", "http://localhost:8080"]
 
 # CORS 추가
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=['*'],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
@@ -41,9 +37,8 @@ app.add_middleware(SessionMiddleware, secret_key=os.getenv("SECRET_KEY"))
 app.include_router(auth_router)
 app.include_router(crop_router)
 app.include_router(chat_router)
-
+app.include_router(predict_router)
 
 if __name__ == "__main__":
     _init_db()
-    uvicorn.run("main:app", host='127.0.0.1', port=8000, reload=False)
-
+    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=False)
